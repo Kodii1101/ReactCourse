b6dd0efca09eb1bc7cf83c05e07337e0b22c3462
Jonnell Lazaro J. Bingat <jlbversusxiii@gmail.com>
2024-02-15 05:29:38 +0800

    finished index.html, moving on to post.html block section

--- assets/js/main.js
+++ assets/js/main.js
@@ -59,4 +59,24 @@ themeToggleBtn.addEventListener('click', () => {
         localStorage.removeItem('currentTheme');
     }
 });
-
+// console.log("ime here")
+// SWIPER
+const swiper = new Swiper('.swiper',{
+    slidesPerView: 1,
+    spaceBetween: 20,
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+        el: '.swiper-pagination'
+    },
+    breakpoints: {
+        700: {
+            slidesPerView: 2
+        },
+        1200: {
+            slidesPerView: 3
+        }
+    }
+});
